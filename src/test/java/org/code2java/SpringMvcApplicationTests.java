5da88748336f46c83134f5fd2f6aa1e769093a4b
Anurag.Prajapati <Anurag.Prajapati@10.0.75.1>
2019-03-28 16:55:23 +0530

    commit

---
+++ src/test/java/org/code2java/SpringMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package org.code2java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class SpringMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
